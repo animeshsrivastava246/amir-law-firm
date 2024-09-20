f1ceb03a05e0ed36c7ead4204de21f6dc6e9bbd5
kushagra-aa <kushagraagnihotri24@gmail.com>
2024-09-20 22:04:14 +0530

    fixed folder structure : pages

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
-import Home from "./components/sections/Home";
-import AboutUs from "./components/sections/AboutUs";
-import PracticeAreas from "./components/sections/PracticeAreas";
-import Blogs from "./components/sections/Blogs";
-import ContactUs from "./components/sections/ContactUs";
-import PrivacyPolicy from "./components/sections/PrivacyPolicy";
-import TermsAndConditions from "./components/sections/TermsAndConditions";
-import Careers from "./components/sections/Careers";
+import Home from "./components/pages/Home";
+import AboutUs from "./components/pages/AboutUs";
+import PracticeAreas from "./components/pages/PracticeAreas";
+import Blogs from "./components/pages/Blogs";
+import ContactUs from "./components/pages/ContactUs";
+import PrivacyPolicy from "./components/pages/PrivacyPolicy";
+import TermsAndConditions from "./components/pages/TermsAndConditions";
+import Careers from "./components/pages/Careers";
 
 const App = () => {
-	return (
-		<Router>
-			<div className="flex flex-col min-h-screen">
-				<Header />
-				<main className="flex-grow">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about" element={<AboutUs />} />
-						<Route path="/blogs" element={<Blogs />} />
-						<Route path="/careers" element={<Careers />} />
-						<Route path="/contact" element={<ContactUs />} />
-						<Route path="/practice-areas" element={<PracticeAreas />} />
-						<Route path="/privacy-policy" element={<PrivacyPolicy />} />
-						<Route path="/terms-conditions" element={<TermsAndConditions />} />
-					</Routes>
-				</main>
-				<Footer />
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/practice-areas" element={<PracticeAreas />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-conditions" element={<TermsAndConditions />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import Clients from "./../sections/Clients";
+import Testimony from "./../sections/Testimony";
+import CarouselData from "../../data/carousel.json";
+import AboutUsHome from "./../sections/AboutUsHome";
+import BlogsHome from "./../sections/BlogsHome";
+import PracticeAreasHome from "./../sections/PracticeAreasHome";
+import Teams from "./../sections/Teams";
+
+const Home = () => {
+  const [carousels, setCarousels] = useState([]);
+
+  useEffect(() => {
+    setCarousels(CarouselData);
+  }, []);
+
+  return (
+    <main>
+      {/* Hero Section with Swiper Carousel */}
+      <section className="relative bg-gray-800 overflow-hidden">
+        <Swiper
+          modules={[Autoplay, Navigation]}
+          spaceBetween={0}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{ delay: 2000 }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          className="w-full h-[50vh] sm:h-[60vh] lg:h-[70vh] relative"
+        >
+          {carousels.map((carousel, index) => (
+            <SwiperSlide key={index} className="relative">
+              <img
+                src={require(`../../assets/banners/${carousel.banner}`)}
+                alt={`${carousel.description}`}
+                className="w-full h-full object-contain" // Use object-contain to fit image within container
+              />
+              <div className="absolute bottom-4 left-4 bg-black bg-opacity-60 text-white p-2 rounded">
+                <p className="text-2xl font-semibold">{`${carousel.description}`}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+          {/* Navigation Buttons */}
+          <div className="swiper-button-prev !text-white !bg-gray-800 !bg-opacity-40 p-4 rounded-full" />
+          <div className="swiper-button-next !text-white !bg-gray-800 !bg-opacity-40 p-4 rounded-full" />
+        </Swiper>
+      </section>
+
+      {/* About Us Section */}
+      <section className="my-12">
+        <AboutUsHome />
+      </section>
+
+      {/* Practice Areas Section */}
+      <section className="my-12">
+        <PracticeAreasHome />
+      </section>
+
+      {/* Blogs Section */}
+      <section className="my-12">
+        <BlogsHome />
+      </section>
+
+      {/* Team Section */}
+      <section className="my-12">
+        <Teams />
+      </section>
+
+      {/* Clients Section */}
+      <section className="my-12">
+        <Clients />
+      </section>
+
+      {/* Testimony Section */}
+      <section className="my-12">
+        <Testimony />
+      </section>
+
+      {/* Google Map Embed */}
+      <section className="my-12">
+        <div className="relative w-full h-[35vh]">
+          <iframe
+            title="Office Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14236.060212578626!2d81.00129866504774!3d26.87126282674772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399be2dd108aa913%3A0xb35ae5f92f7d1a2e!2sHigh%20Court%20Lucknow!5e0!3m2!1sen!2sin!4v1726222221107!5m2!1sen!2sin"
+            className="w-full h-full border-0"
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
